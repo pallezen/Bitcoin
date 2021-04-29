f7e92c4dd081da7a0bfc4d92fbe9281f70368eba
PerIskogen <83424426+PerIskogen@users.noreply.github.com>
2021-04-29 20:45:43 +0200

    Update Visuals.py

--- Visuals.py
+++ Visuals.py
@@ -1,34 +1,55 @@
-# import the pygame module, so you can use it
+# Import the pygame module
 import pygame
- 
-# define a main function
-def main():
-     
-    # initialize the pygame module
-    pygame.init()
-    # load and set the logo
-    logo = pygame.image.load("logo32x32.png")
-    pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
-     
-    # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240,180))
-     
-    # define a variable to control the main loop
-    running = True
-     
-    # main loop
-    while running:
-        # event handling, gets all event from the event queue
-        for event in pygame.event.get():
-            # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Initialize pygame
+pygame.init()
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Variable to keep the main loop running
+running = True
+
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
                 running = False
-     
-     
-# run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
-if __name__=="__main__":
-    # call the main function
-    main()
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
+
+            # Fill the screen with white
+screen.fill((255, 255, 255))
+
+# Create a surface and pass in a tuple containing its length and width
+surf = pygame.Surface((50, 50))
+
+# Give the surface a color to separate it from the background
+surf.fill((0, 0, 0))
+rect = surf.get_rect()
+
+# This line says "Draw surf onto the screen at the center"
+screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+pygame.display.flip()
