a4402511f1c55db19e97be55c6c15908b51f1a03
pallezen <phegre@hotmail.com>
2021-05-13 19:59:11 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,20 +6,30 @@ class Person:
         self.info = "Private key: " + str(self.privateKey) + " Public Key: " + str(self.publicKey) + " Coins: " + str(self.coins)
 
 class Transaction:
-    def __init__(self, p1PubKey, hashPrevTx, p0Sign, id):
+    def __init__(self, p1PubKey, hashPrevTx, p0Sign, coins, id):
         self.p1PubKey = p1PubKey
         self.hashPrevTx = hashPrevTx
         self.p0Sign = p0Sign    # p0's private key used to hash information verifiable by its public key
+        self.coins = coins
         self.id = id
+        self.info = "Person 1 public key: " + str(self.p1PubKey) + " Hash of previous transaction: " + str(self.hashPrevTx) + " Person 0 signature: " + str(self.p0Sign) + " Coins transfered: " + str(self.coins) + "Transaction ID: " + str(self.id)
+# Run a function to complete coin transfer here? E.g:
+# ray.coins += tx50.coins
+# kal.coins -= tx50.coins
 
+# Convert to hex when certain hash function is functional
 def hash(n):
     return n*9876+7
 
 kal = Person(1000, 1/1000, 100)
 ray = Person(1/50, 50, 20)
 
-tx50 = Transaction(ray.publicKey, 2345, hash(kal.privateKey), 50)
-tx51 = Transaction(kal.publicKey, hash(tx50.id), hash(ray.privateKey), tx50.id+1)
+tx50 = Transaction(ray.publicKey, 2345, hash(kal.privateKey), 15, 50)
+tx51 = Transaction(kal.publicKey, hash(tx50.id), hash(ray.privateKey), 2, tx50.id+1)
 
 print(tx51.hashPrevTx)
 print(kal.info)
+print(tx51.info)
+
+# Next:
+# Get transaction to apply changes to wallets
